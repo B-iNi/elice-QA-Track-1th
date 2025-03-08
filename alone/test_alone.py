7713c02631048504b20f567d7e9e1ee3319d294c
B-iNi <bini392573@gmail.com>
2025-03-08 15:54:32 +0900

    Add files via upload

--- alone/test_alone.py
+++ alone/test_alone.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 
 
 #def add(a,b):
@@ -11,15 +12,50 @@ import unittest
 #if __name__ == '__main__':
 #    unittest.main()
 
-class TestMathOperations(unittest.TestCase):
-    def setUp(self):
+#class TestMathOperations(unittest.TestCase):
+ #   def setUp(self):
 
-        self.a = 2
-        self.b = 3
+  #      self.a = 2
+   #     self.b = 3
 
-    def test_add(self):
-        self.assertEqual(self.a + self.b, 5)
+    #def test_add(self):
+     #   self.assertEqual(self.a + self.b, 5)
 
-    def tearDown(self):
+    #def tearDown(self):
 
-        pass
+     #   pass
+
+#def add_review(food, star, comment):
+ #   if not (1 <= star <= 5):
+  #      raise ValueError("벌점은 1~5점 사이야 함")
+   # if len(comment) <5:
+    #    raise ValueError("리뷰는 최소 5자 이상이어야 함")
+    #return f"{food}: {'★'*star} - {comment}"
+    
+#print(add_review("피자", 5, "정말 맛있어요!"))
+
+#def add(d,b):
+#    return a+b
+
+#def test_add():
+ #   assert add(2,3) == 5
+  #  assert add(-1,1) == 0
+
+from coupon import apply_coupon
+
+
+    
+    
+
+def test_apply_valid_coupon():
+    assert apply_coupon(10000, 20) == 8000
+
+def test_apply_zero_coupon():
+    assert apply_coupon(10000, 0) == 10000
+
+def test_apply_full_coupon():
+    assert apply_coupon(10000, 100) == 0
+
+def test_invalid_coupon_rate():
+    with pytest.raises(ValueError):
+        apply_coupon(10000, 101)
